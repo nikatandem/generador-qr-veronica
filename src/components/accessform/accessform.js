5aa450e05b870263e9d0739a955c4847753b39ff
nikatandem <nikatandem@gmail.com>
2024-05-31 12:38:38 +0200

    formulario acceso

--- src/components/accessform/accessform.js
+++ src/components/accessform/accessform.js
@@ -1,41 +1,50 @@
 import React from "react";
+import "./formstyle.css";
 import { useState } from "react";
 
-
 function Acceso() {
-    const [nombre, setNombre] = useState(''); 
-    const [password, setPassword] = useState(''); 
-    const [enviado, setEnviado] = useState(false); // 
+  const [nombre, setNombre] = useState("");
+  const [password, setPassword] = useState("");
+  const [enviado, setEnviado] = useState(false); //
 
-    const handleChangeName = (event) => {
-        setNombre(event.target.value);
-    };
+  const handleChangeName = (event) => {
+    setNombre(event.target.value);
+  };
 
-    const handleChangePassword = (event) => {
-        setPassword(event.target.value);
-    };
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
-
-    return (
-
-        <div>
-            <h1>Acceso para empleados</h1>
-            <form>
+  return (
+ <div class="container">   
+  
+    
+    <div className="acceso">
+     
+     <div>
+        <form>
+            <h1 className="titulo">Acceso para empleados</h1>
                 <div>
-                    <label> Usuario:
-                        < br/> <input type="text" value={nombre} onChange={handleChangeName} />
+                    <label className="contraseña"> Usuario:
+                        < br/> <input className="campo"  type="text" value={nombre} onChange={handleChangeName} />
                     </label>
                 </div>
                 <div>
-                    <label>Contraseña:
-                    < br/><input type="password" value={password} onChange={handleChangePassword} />
+                    <label className="contraseña">Contraseña:
+                    <br/><input className="campo" type="password" value={password} onChange={handleChangePassword} />
                     </label>
+                    
                 </div>
-                <button type="submit" disabled={enviado} >Acceder</button>
+                <button className="elemento-button" type="submit" disabled={enviado} >Acceder</button>
+                <p className="p-recuperar">¿Has olvidado tu contraseña?</p>
+                    <button className="btn-recuperar">Recuperar clave de acceso</button>
             </form>
+          
         </div>
-    );
+
+
+      </div>
+      </div>
+  );
 }
-
 export default Acceso;
-
