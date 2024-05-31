1b687213a0bdd2e5cb057801aaf1b91d1782bc16
nikatandem <nikatandem@gmail.com>
2024-05-31 09:43:51 +0200

    estilos de btn y form

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,14 +5,14 @@ function Footer(){
 return(
     <footer className="footer"
     style={{
-      fontSize: `var(--font-sm)`,
+      fontSize: `var(--font-md)`,
     }}
   >
     
-    <div><a href="https://www.patrimonionacional.es/accesibilidad"> Accesibilidad|</a> <a href="https://www.patrimonionacional.es/politica-de-privacidad"> Política de privacidad|</a> <a href="https://www.patrimonionacional.es/aviso-legal"> Aviso Legal|</a> <a href="https://www.patrimonionacional.es/politica-de-cookies">Política de Cookies</a> </div>
-    <div>© {new Date().getFullYear()} &middot; TandEM Aranjuez, todos los derechos reservados.
+    <div className="foot"><a href="https://www.patrimonionacional.es/accesibilidad"> Accesibilidad|</a> <a href="https://www.patrimonionacional.es/politica-de-privacidad"> Política de privacidad|</a> <a href="https://www.patrimonionacional.es/aviso-legal"> Aviso Legal|</a> <a href="https://www.patrimonionacional.es/politica-de-cookies">Política de Cookies</a> </div>
+    <div className="foot">© {new Date().getFullYear()} &middot; TandEM Aranjuez, todos los derechos reservados.
     {` `}</div>
-    <div><FooterLogo/></div>
+    <div className="foot"><FooterLogo/></div>
     
   </footer>)
 }
