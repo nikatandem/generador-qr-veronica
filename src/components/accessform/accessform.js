a6adb761a7f491d34357e869cf935d777e0ba598
nikatandem <nikatandem@gmail.com>
2024-06-04 09:13:56 +0200

    estilos form

--- src/components/accessform/accessform.js
+++ src/components/accessform/accessform.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./formstyle.css";
-import { useState } from "react";
 
 function Acceso() {
   const [nombre, setNombre] = useState("");
   const [password, setPassword] = useState("");
-  const [enviado, setEnviado] = useState(false); //
+  const [enviado, setEnviado] = useState(false);
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [email, setEmail] = useState("");
 
   const handleChangeName = (event) => {
     setNombre(event.target.value);
@@ -15,36 +16,57 @@ function Acceso() {
     setPassword(event.target.value);
   };
 
+  const handleRecoverPasswordClick = () => {
+    setAlertVisible(true);
+  };
+
+  const handleSubmitEmail = (event) => {
+    event.preventDefault();
+    setAlertVisible(false);
+    alert("Revise su correo electrónico, le hemos enviado un enlace para cambiar su contraseña.");
+  };
+
+  const onDismiss = () => setAlertVisible(false);
+
   return (
- <div class="container">   
+    <div className="container">
+      <div className="custom-box">
+      <form className="acceso">
+        <h1 className="titulo">Acceso para empleados</h1>
+        <div className="datos">
+          <label className="label-1" > Usuario:
+<br />
+      <input className="campo" type="text" value={nombre} onChange={handleChangeName} placeholder="E-mail"/>
+        </label>
+      <label className="label-1">Contraseña:
+<br />
+      <input className="campo" type="password" value={password} onChange={handleChangePassword} placeholder="Contraseña"/>
+        </label>
+          </div>
+      <div className="botones">
+   <button className="elemento-button" type="submit" disabled={enviado}>Acceder</button>
+      <p className="p-recuperar">¿Has olvidado tu contraseña?</p>
+      <button type="button" className="btn-recuperar" onClick={handleRecoverPasswordClick}>Recuperar clave de acceso</button>
+</div>
+   
+    </form>
+    </div>
+{alertVisible && (
+      
+<div className="custom-alert">
+  <form onSubmit={handleSubmitEmail} className="custom-form">
+   <button type="button" className="close-button" onClick={onDismiss}>x</button>
+  <label htmlFor="email" className="form-label">Introduzca su correo electrónico:</label>
+    <input type="email" name="email" id="email" className="form-input" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
+
   
-    
-    <div className="acceso">
-     
-     <div>
-        <form>
-            <h1 className="titulo">Acceso para empleados</h1>
-                <div>
-                    <label className="contraseña"> Usuario:
-                        < br/> <input className="campo"  type="text" value={nombre} onChange={handleChangeName} />
-                    </label>
-                </div>
-                <div>
-                    <label className="contraseña">Contraseña:
-                    <br/><input className="campo" type="password" value={password} onChange={handleChangePassword} />
-                    </label>
-                    
-                </div>
-                <button className="elemento-button" type="submit" disabled={enviado} >Acceder</button>
-                <p className="p-recuperar">¿Has olvidado tu contraseña?</p>
-                    <button className="btn-recuperar">Recuperar clave de acceso</button>
-            </form>
-          
-        </div>
-
-
-      </div>
-      </div>
+  <button type="submit" className="confirmar">Confirmar</button> 
+  </form>
+</div>
+      )}
+    </div>
   );
+
 }
+
 export default Acceso;
