b726eb7aad43bfd2cbdc82dd54197351e2ef8300
nikatandem <nikatandem@gmail.com>
2024-06-13 08:26:09 +0200

    cambios mapa

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,14 @@
-
-// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-//     if (stage === 'build-html') {
-//       actions.setWebpackConfig({
-//         module: {
-//           rules: [
-//             {
-//               test: /react-leaflet|leaflet/,
-//               use: loaders.null(),
-//             },
-//           ],
-//         },
-//       });
-//     }
-//   };
-  
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === 'build-html') {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /react-leaflet|leaflet/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
--- src/components/generador-qr/mapa.js
+++ src/components/generador-qr/mapa.js
@@ -1,44 +1,67 @@
-// src/components/MapaConMarcador.js
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, useMap, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer,  useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
 
-import iconUrl from 'leaflet/dist/images/marker-icon.png';
-import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
-import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+const MapaConMarcador = ({ setLatLng }) => {
+  const [isClient, setIsClient] = useState(false);
 
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-    iconUrl,
-    iconRetinaUrl,
-    shadowUrl,
-});
-const LocationMarker = ({ setLatLng }) => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsClient(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      const L = require('leaflet');
+
+      // Arreglar el problema de los iconos que no se muestran
+      import('leaflet/dist/images/marker-icon.png');
+      import('leaflet/dist/images/marker-icon-2x.png');
+      import('leaflet/dist/images/marker-shadow.png');
+
+      delete L.Icon.Default.prototype._getIconUrl;
+
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
+        iconUrl: require('leaflet/dist/images/marker-icon.png').default,
+        shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+      });
+    }
+  }, [isClient]);
+
+  const LocationMarker = ({ setLatLng }) => {
     const map = useMap();
+
     useEffect(() => {
       const handleClick = (e) => {
         setLatLng(e.latlng);
       };
+
       map.on('click', handleClick);
+
       // Limpiar el evento cuando el componente se desmonte
       return () => {
         map.off('click', handleClick);
       };
     }, [map, setLatLng]);
+
     return null;
   };
-const MapaConMarcador = ({ setLatLng }) => {
+
   return (
-    <MapContainer center={[40.030501, -3.604052]} zoom={13} style={{ height: '50vh', width: '100%' }}>
-        <TileLayer
+    <>
+      {isClient && (
+        <MapContainer center={[40.030501, -3.604052]} zoom={13} style={{ height: '50vh', width: '100%' }}>
+          <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <LocationMarker setLatLng={setLatLng} />
-    </MapContainer>
-);
+          />
+          <LocationMarker setLatLng={setLatLng} />
+        </MapContainer>
+      )}
+    </>
+  );
 };
 
 export default MapaConMarcador;
-
