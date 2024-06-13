ce336a91f9a625d85f68d300f031aeb4528a8d0a
nikatandem <nikatandem@gmail.com>
2024-06-13 09:40:13 +0200

    arreglar problema iconos mapa

--- src/components/generador-qr/mapa.js
+++ src/components/generador-qr/mapa.js
@@ -1,6 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer,  useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+// Arreglar el problema de los iconos que no se muestran
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl,
+    iconRetinaUrl,
+    shadowUrl,
+});
+const LocationMarker = ({ setLatLng }) => {
+    const map = useMap();
+    useEffect(() => {
+      const handleClick = (e) => {
+        setLatLng(e.latlng);
+      };
+      map.on('click', handleClick);
+      // Limpiar el evento cuando el componente se desmonte
+      return () => {
+        map.off('click', handleClick);
+      };
+    }, [map, setLatLng]);
+    return null;
+  };
 
 const MapaConMarcador = ({ setLatLng }) => {
   const [isClient, setIsClient] = useState(false);
