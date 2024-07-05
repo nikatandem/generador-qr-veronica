9abe560dfc57b0fb21ace432a4c7dbfc0d147078
nikatandem <nikatandem@gmail.com>
2024-07-05 12:07:10 +0200

    browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // Importa los estilos de Font Awesome
 import { config } from "@fortawesome/fontawesome-svg-core"; // Importa la configuración de Font Awesome
 
+data: () => ({
+    showMessage: process.isClient
+        ? !localStorage.getItem("hideMessage")
+        : false,
+})
